6811f4306b3bd2c89eefe1f6e7f9c353de5852a8
Clebson Farias <clebson.farias@ctec.ufal.br>
2021-06-16 17:02:22 -0300

    Create model and admin

--- api_telemed/models.py
+++ api_telemed/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone as tz
+from accounts.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(verbose_name='Nome', max_length=255, null=True, default=None)
+    description = models.TextField(verbose_name='Descrição', null=True)
+    price = models.DecimalField(verbose_name='Preço', max_digits=6, decimal_places=2)
+    created_date = models.DateField(verbose_name='Data de criação', default=tz.now)
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(verbose_name='Produto', to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(verbose_name='Quantidade', default=1)
+    price = models.DecimalField(verbose_name='Preço', max_digits=8, decimal_places=2)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(verbose_name='Usuário', to=User,
+                             related_name='order_user', on_delete=models.CASCADE
+                             )
+    cart_item = models.ForeignKey(verbose_name='Carrinho', to=CartItem,
+                                  related_name='order_item', on_delete=models.CASCADE)
