d4018f9d59e5c1edc54e3c2d8ba9ee190cfae47f
Clebson Farias <clebson.farias@ctec.ufal.br>
2021-06-16 22:46:37 -0300

    Search ok

--- api_telemed/views.py
+++ api_telemed/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from datetime import datetime
+
 
 from .models import Product, Order
-from .serializers import ProductSerializer, OrderSerializer
+from .serializers import ProductSerializer, OrderSerializer, OrderSerializerSearch
 
 
 # Create your views here.
@@ -16,6 +18,7 @@ class ProductViewSet(mixins.CreateModelMixin,
 
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    permission_classes = (IsAuthenticated, )
 
 
 class OrderViewSet(mixins.CreateModelMixin,
@@ -25,13 +28,13 @@ class OrderViewSet(mixins.CreateModelMixin,
 
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
+    permission_classes = (IsAuthenticated, )
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
 
     def create(self, request, *args, **kwargs):
         user_id_data = request.data['user']
-        print(request.data)
         if request.user.id != user_id_data:
             return Response(data={'message': 'user id passed in body different from logged user id!'})
         
@@ -39,4 +42,15 @@ class OrderViewSet(mixins.CreateModelMixin,
 
     @action(methods=['get'], detail=False)
     def search(self, request, pk=None):
-        return None
+        start_date = request.query_params.get('start_date')
+        end_date = request.query_params.get('end_date')
+
+        if start_date:
+            start_date = datetime.strptime(start_date, '%d/%m/%Y')
+            self.queryset = self.queryset.filter(created_date__gte=start_date)
+
+        if end_date:
+            end_date = datetime.strptime(end_date, '%d/%m/%Y')
+            self.queryset = self.queryset.filter(created_date__lte=end_date)
+
+        return super(OrderViewSet, self).list(request=request)
